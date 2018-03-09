7343e3f9a0bfa4c26ba3da2e0c7e423e59ba7775
Liu-Wen-Yuan <15816141883@163.com>
2018-03-09 17:49:50 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,6 +3,6 @@ var Base64=prompt("The Base64 Content")
 var MIMEType=prompt("The MIME Type")
 if(Base64!=null&&MIMEType!=null){
 var url="data:"+MIMEType+";base64,"+Base64;
-  document.write(url)
+  document.write("<a href='"+url+"'>"+url+"</a>")
   location.href=url
 }else{alert("Error!")}
